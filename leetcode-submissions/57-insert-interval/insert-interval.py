58fa3fd4766d27ad8519896a2122b3620b8086e8
KamalMaharana <40832769+KamalMaharana@users.noreply.github.com>
2023-09-06 21:27:17 +0530

    Time: 91 ms (38.18%) | Memory: 19.9 MB (42.02%) - LeetSync

---
+++ leetcode-submissions/57-insert-interval/insert-interval.py
@@ -0,0 +1,16 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res, i = [], -1
+        I = newInterval
+        for i, (x, y) in enumerate(intervals):
+            if y < I[0]:
+                res.append([x, y])
+            elif I[1] < x:
+                i -= 1
+                break
+            else:
+                I[0] = min(I[0], x)
+                I[1] = max(I[1], y)
+                
+        return res + [I] + intervals[i+1:]
+        
