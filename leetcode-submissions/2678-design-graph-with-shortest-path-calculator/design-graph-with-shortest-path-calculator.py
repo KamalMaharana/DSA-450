56463b14c1b93fab496395f8d6f2d693b106e2af
KamalMaharana <40832769+KamalMaharana@users.noreply.github.com>
2023-11-12 02:24:45 +0530

    Time: 540 ms (89.53%) | Memory: 19.7 MB (20.35%) - LeetSync

---
+++ leetcode-submissions/2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.py
@@ -0,0 +1,36 @@
+class Graph:
+    def __init__(self, n: int, edges: List[List[int]]):
+        self.graph = defaultdict(list)
+        self.nodes = n
+        for u, v, cost in edges:
+            self.graph[u] += [(v, cost)]
+
+    def addEdge(self, edge: List[int]) -> None:
+        u, v, cost = edge
+        self.graph[u] += [(v, cost)]
+        self.nodes += 1
+        
+    def shortestPath(self, node1: int, node2: int) -> int:
+        if not self.nodes: return -1
+        heap = []
+        heappush(heap, (0, node1))
+        dist = [inf] * self.nodes
+        dist[node1] = 0
+        while heap:
+            cost, node = heappop(heap)
+            if node == node2:
+                return cost
+            if cost > dist[node]:
+                continue
+
+            for baju_wala, new_cost in self.graph[node]:
+                new_cost = cost + new_cost
+                if new_cost < dist[baju_wala]:
+                    dist[baju_wala] = new_cost
+                    heappush(heap, (new_cost, baju_wala))
+        return -1
+
+# Your Graph object will be instantiated and called as such:
+# obj = Graph(n, edges)
+# obj.addEdge(edge)
+# param_2 = obj.shortestPath(node1,node2)
