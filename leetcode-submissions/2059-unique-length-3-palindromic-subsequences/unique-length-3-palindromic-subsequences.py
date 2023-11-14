6d6c80954daa0d640fef7f2ba4a4be7643f2cad6
KamalMaharana <40832769+KamalMaharana@users.noreply.github.com>
2023-11-15 01:37:17 +0530

    Time: 1584 ms (41.91%) | Memory: 17.2 MB (70.79%) - LeetSync

---
+++ leetcode-submissions/2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        result = 0
+        n = len(s)
+        for i in range(26):
+            start = None
+            end = None
+            chars = set()
+            ch = chr(i + ord('a'))
+            i = 0
+            j = len(s) - 1
+            while i < n:
+                if s[i] == ch:
+                    break
+                i += 1
+
+            while j >= 0:
+                if s[j] == ch:
+                    break
+                j -= 1
+            
+            for k in range(i + 1, j):
+                chars.add(s[k])
+            result += len(chars)
+        return result
