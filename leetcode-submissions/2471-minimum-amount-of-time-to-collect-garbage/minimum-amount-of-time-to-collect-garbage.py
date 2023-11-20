3f3655f90851351524b45cbcd76fbaf01b0008c9
KamalMaharana <40832769+KamalMaharana@users.noreply.github.com>
2023-11-20 23:22:59 +0530

    Time: 882 ms (67.27%) | Memory: 39.3 MB (34.21%) - LeetSync

--- leetcode-submissions/2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.py
+++ leetcode-submissions/2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.py
@@ -2,21 +2,22 @@ class Solution:
     def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
         prefix_sum = []
         prev = 0
-        # [2,4,3]
-        # [0,2,6,9]
         for i in travel:
             prefix_sum.append(prev)
             prev += i
         prefix_sum.append(prev)
-        # print(prefix_sum)
         res = 0
-        truck = defaultdict(int)
+        truck = [0] * 3
         for i in range(len(garbage)):
             res += len(garbage[i])
             for c in garbage[i]:
-                truck[c] = i
-        
-        for c in truck:
-            index = truck[c]
+                if c == "M":
+                    truck[0] = i
+                elif c == "P":
+                    truck[1] = i
+                else:
+                    truck[2] = i
+        for i in range(len(truck)):
+            index = truck[i]
             res += prefix_sum[index]
         return res
