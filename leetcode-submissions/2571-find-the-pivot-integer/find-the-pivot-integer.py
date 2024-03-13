6a06fe4ccd0ee99bdf262a831465d7ecc1df3f13
KamalMaharana <40832769+KamalMaharana@users.noreply.github.com>
2024-03-13 22:47:03 +0530

    Time: 45 ms (64.82%) | Memory: 16.5 MB (92.38%) - LeetSync

--- leetcode-submissions/2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ leetcode-submissions/2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -1,10 +1,7 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        def get_sum(n):
-            return (n * (n + 1))//2
-        total_sum = get_sum(n)
+        total_sum = (n * (n + 1))//2
         curr_sum = 0
-        # 1 2 3 4 5 6 7 8 = 45 - 
         for i in range(1, n + 1):
             curr_sum += i
             target = (total_sum - curr_sum) + i
