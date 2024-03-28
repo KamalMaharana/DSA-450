31bba24004d6ee58e8b280ea8689dbf37514db21
KamalMaharana <40832769+KamalMaharana@users.noreply.github.com>
2024-03-28 22:49:21 +0530

    Time: 1174 ms (43.15%) | Memory: 31.1 MB (74.68%) - LeetSync

--- leetcode-submissions/3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
+++ leetcode-submissions/3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
@@ -8,13 +8,10 @@ class Solution:
         while right < len(nums):
             n = nums[right]
             freq[n] += 1
-            # curr_max = max(curr_max, freq[n])
-            # print(freq)
             while freq[n] > k:
                 n2 = nums[left]
                 freq[n2] -= 1
                 left += 1
             res = max(res, right - left + 1)
-            # print(res, left, right, freq)
             right += 1
         return res
