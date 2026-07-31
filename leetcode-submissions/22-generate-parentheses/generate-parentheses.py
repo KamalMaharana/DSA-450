20eb1d82a18c8eba60a27bcb8cbdf7974ae84a57
KamalMaharana <40832769+KamalMaharana@users.noreply.github.com>
2026-07-31 11:13:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (99.26%) - LeetSync

--- leetcode-submissions/22-generate-parentheses/generate-parentheses.py
+++ leetcode-submissions/22-generate-parentheses/generate-parentheses.py
@@ -1,16 +1,16 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        self.res = []
+        # self.res = []
         def dp(o, c, s):
             if o > n or c > n or c > o:
                 return
             
             if o == n and c == n:
-                self.res.append(s)
+                res.append(s)
                 return
             
             dp(o + 1, c, s + '(')
             dp(o, c + 1, s + ')')
-        
+        res = []
         dp(1, 0, '(')
-        return self.res
+        return res
