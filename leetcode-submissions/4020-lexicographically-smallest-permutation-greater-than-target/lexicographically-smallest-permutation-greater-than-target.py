466c383869131a7cbf5ffd50701af816add43896
Kamalkant Maharana <40832769+KamalMaharana@users.noreply.github.com>
2026-08-27 12:46:26 +0530

    Time: 75 ms (5.26%) | Memory: 19.5 MB (21.05%) - LeetSync

--- leetcode-submissions/4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.py
+++ leetcode-submissions/4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.py
@@ -1,39 +1,71 @@
 from collections import Counter
-
-
 class Solution:
 
   def lexGreaterPermutation(self, s: str, target: str) -> str:
     n = len(s)
-    total_counts = Counter(s)
+    freq = Counter(s)
+    strings = []
+    
+    ch = ""
+    for f in freq:
+        if f > target[0]:
+            if ch == "":
+                ch = f
+            else:
+                ch = min(ch, f)
+    
+    # print(ch)
+    if ch != "":
+        freq[ch] -= 1
+        temp = ""
+        for f in freq:
+            temp += (f * freq[f])
+        
+        temp = sorted(temp)
+        temp = "".join(temp)
+        # print(ch + temp)
+        strings.append(ch + temp)
+        freq[ch] += 1
+    
+    
+    for i, c in enumerate(target):
+        if freq[c] > 0:
+            freq[c] -= 1
+            flag = 0
+            temp_s = target[0:i+1]
+            # print("Temp", temp_s, "Char", c)
+            ch = ""
+            if i + 1 < n:
+                for f in freq:
+                    if f > target[i + 1] and freq[f] > 0:
+                        if ch == "":
+                            ch = f
+                        else:
+                            ch = min(ch, f)
+                        flag = 1
+                # print(ch)
+                temp_s += ch
+                freq[ch] -= 1
+            # print("Temp_S 48", temp_s)
+            # print(i, c, ch)
+            if flag:
+                temp = ""
+                for f in freq:
+                    temp += f * freq[f]
+                
+                temp = sorted(temp)
+                temp_s = temp_s + "".join(temp)
+                # print(temp_s, ch, i+1, c)
+                strings.append(temp_s)
 
-    # 1. Determine how far we can match target exactly
-    prefix_counts = [Counter()]
-    matched_len = 0
-    cur_counts = Counter()
+            freq[ch] += 1
+        else:
+            break
 
-    for ch in target:
-      if cur_counts[ch] < total_counts[ch]:
-        cur_counts[ch] += 1
-        prefix_counts.append(cur_counts.copy())
-        matched_len += 1
-      else:
-        break
-
-    # 2. Iterate backwards from matched_len down to 0 to find divergence point
-    for i in range(matched_len, -1, -1):
-      # Available characters if we match target[0...i-1]
-      used = prefix_counts[i]
-      avail = total_counts - used
-
-      if i < n:
-        # Find the smallest character strictly greater than target[i]
-        for ch_code in range(ord(target[i]) + 1, ord("z") + 1):
-          ch = chr(ch_code)
-          if avail[ch] > 0:
-            avail[ch] -= 1
-            # Construct suffix from remaining characters sorted
-            suffix = "".join(sorted(avail.elements()))
-            return target[:i] + ch + suffix
-
+    # print(strings)
+    if len(strings):
+        strings.sort()
+        return strings[0]
     return ""
+    
+                
