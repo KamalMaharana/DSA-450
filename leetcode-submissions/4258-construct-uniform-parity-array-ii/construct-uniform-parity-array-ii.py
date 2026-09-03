d4ef4b6880cb10e295486d34ec01c8694b618fd3
Kamalkant Maharana <40832769+KamalMaharana@users.noreply.github.com>
2026-09-03 21:53:07 +0530

    Time: 93 ms (27.44%) | Memory: 35.5 MB (18.90%) - LeetSync

---
+++ leetcode-submissions/4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        nums1.sort()
+        even_possible = False
+        odd_possible = False
+        # All evens
+        odd = 1 if nums1[0] % 2 == 1 else 0
+        for n in nums1[1:]:
+            if n&1:
+                if not odd:
+                    return False
+                odd += 1
+        
+        return True
